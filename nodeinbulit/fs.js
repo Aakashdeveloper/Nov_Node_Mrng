1989fb75c5e6113242a7549fce1c7286758b1f07
Aakashdeveloper <ahanda205@gmail.com>
2020-11-05 08:34:24 +0530

    a

--- nodeinbulit/fs.js
+++ nodeinbulit/fs.js
@@ -11,9 +11,21 @@ fs.appendFile('mytext.txt','This is my content \n',function(err){
     if(err) throw err;
     console.log('File appeneded')
 });
-*/
+
 //read file
 fs.readFile('mytext.txt','utf-8',function(err,data){
     if(err) throw err;
     console.log(data)
+});
+
+//rename
+fs.rename('mytext.txt','myfile.txt',function(err){
+    if(err) throw err;
+    console.log("File Renamed")
+})
+*/
+//delete
+fs.unlink('mytext.txt',function(err){
+    if(err) throw err;
+    console.log("File Deleted")
 })
